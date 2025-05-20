b9ff5ba9aa5aea14017c296ffba00a68f5f20c47
James Hodgkinson <james@terminaloutcomes.com>
2025-05-21 08:52:06 +1000

    fix: tets

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,7 +5,10 @@ from qbtapi import QBTAPIConfig
 
 
 def test_config(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setenv("QB_USERNAME", "testuser")
+    monkeypatch.setenv("QB_PASSWORD", "hunter2")
+    monkeypatch.setenv("HECHOST", "localhost")
+    monkeypatch.setenv("HECTOKEN", "hunter2")
     monkeypatch.setenv("QB_PORT", "12345")
-    monkeypatch.setenv("HEC_HOST", "localhost")
     foo = QBTAPIConfig.model_validate({})
     assert foo.qb_port == 12345
