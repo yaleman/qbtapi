635e358c72c4f1303714eef7bb817458a7986a01
James Hodgkinson <james@terminaloutcomes.com>
2025-05-21 08:51:42 +1000

    fix: tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,5 +6,6 @@ from qbtapi import QBTAPIConfig
 
 def test_config(monkeypatch: pytest.MonkeyPatch) -> None:
     monkeypatch.setenv("QB_PORT", "12345")
+    monkeypatch.setenv("HEC_HOST", "localhost")
     foo = QBTAPIConfig.model_validate({})
     assert foo.qb_port == 12345
