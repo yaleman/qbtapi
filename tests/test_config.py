b949ac42b1e0191e9abb0097851f9bcf7c2ebe94
James Hodgkinson <james@terminaloutcomes.com>
2025-08-04 13:44:01 +1000

    linting tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,10 +5,11 @@ from qbtapi import QBTAPIConfig
 
 
 def test_config(monkeypatch: pytest.MonkeyPatch) -> None:
+    """test config parsing"""
     monkeypatch.setenv("QB_USERNAME", "testuser")
     monkeypatch.setenv("QB_PASSWORD", "hunter2")
     monkeypatch.setenv("HECHOST", "localhost")
     monkeypatch.setenv("HECTOKEN", "hunter2")
     monkeypatch.setenv("QB_PORT", "12345")
-    foo = QBTAPIConfig.model_validate({})
-    assert foo.qb_port == 12345
+    test_parsed_config = QBTAPIConfig.model_validate({})
+    assert test_parsed_config.qb_port == 12345
